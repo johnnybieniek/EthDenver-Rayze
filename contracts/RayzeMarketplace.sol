c1ba43240d3dd680379d890255039846fab3cbfa
Johnny Bieniek <jjbieniek@gmail.com>
2022-12-08 21:28:27 -0500

    Edited the RayzeMeal contract. Built the foundation of the testing script.

--- contracts/RayzeMarketplace.sol
+++ contracts/RayzeMarketplace.sol
@@ -2,27 +2,38 @@
 pragma solidity ^0.8.17;
 
 import {MealToken} from "./MealToken.sol";
+import {RayzeMeal} from "./RayzeMeal.sol";
+
+/// @title RayzeMeal - Tokenized meals which are redeemable
+/// @author Jamshed Cooper, Jan Bieniek
 
 contract RayzeMarketplace {
     MealToken public mealToken;
+    RayzeMeal public rayzeMeal;
 
-    constructor(string memory _tokenName, string memory _tokenSymbol) {
+    constructor(
+        string memory _tokenName,
+        string memory _tokenSymbol,
+        string memory _nftName,
+        string memory _nftSymbol
+    ) {
         mealToken = new MealToken(_tokenName, _tokenSymbol);
+        rayzeMeal = new RayzeMeal(_nftName, _nftSymbol);
     }
 
-/// @dev register the restaurant as a wallet that can issue NFTs
-    function registerRestaurant() public  {
+    /// @dev register the restaurant as a wallet that can issue NFTs
+    function registerRestaurant() public {
         //we should be able to control which restaurant issues NFTs. and should be able to switch them off if required.
     }
-/// @dev Restaurant can create a Rayze Meal NFT of size #of meals - to be minted by eaters
-    function issueRayzeMeal(string memory name, string memory symbol, uint256 size) public  {
-    }
-/// @dev Returns balance of booked meals (# meals that are booked)
-    function balanceBooked(address rayzeMealContract) public  returns (uint256) {
-    }
-/// @dev Returns balance of redeemed meals (MealCoin / USDC that is in escrow)
-    function escrowBalance(address rayzeMealContract) public  returns (uint256) {
-    }
+
+    /// @dev Restaurant can create a Rayze Meal NFT of size #of meals - to be minted by eaters
+    function issueRayzeMeal(string memory name, string memory symbol, uint256 size) public {}
+
+    /// @dev Returns balance of booked meals (# meals that are booked)
+    function balanceBooked(address rayzeMealContract) public returns (uint256) {}
+
+    /// @dev Returns balance of redeemed meals (MealCoin / USDC that is in escrow)
+    function escrowBalance(address rayzeMealContract) public returns (uint256) {}
 }
 
 // RayzeNetwork
@@ -32,6 +43,3 @@ contract RayzeMarketplace {
 // - bookRayzeMeal(RayzeMeal contract address
 //    , Number of meals to buy)
 // - redeemMeal(RayzeMeal contract address)
-
-
-
---
+++ test/rayze-test.ts
@@ -0,0 +1,113 @@
+import { expect } from "chai"
+import { beforeEach, it } from "mocha"
+import { ethers } from "hardhat"
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import {
+    MealToken,
+    MealToken__factory,
+    RayzeMarketplace,
+    RayzeMarketplace__factory,
+    RayzeMeal,
+    RayzeMeal__factory,
+} from "../typechain-types"
+
+const TOKEN_NAME = "Meal Token"
+const TOKEN_SYMBOL = "MTK"
+const NFT_NAME = "RayzeMeal"
+const NFT_SYMBOL = "RML"
+
+describe("Rayze Marketplace", async () => {
+    let accounts: SignerWithAddress[]
+    let rayze: RayzeMarketplace
+    let mealNFT: RayzeMeal
+    let mealToken: MealToken
+    let rayzeContractFactory: RayzeMarketplace__factory
+    let nftContractFactory: RayzeMeal__factory
+    let mealTokenFactory: MealToken__factory
+
+    beforeEach(async () => {
+        accounts = await ethers.getSigners()
+        rayzeContractFactory = await ethers.getContractFactory("RayzeMarketplace")
+        nftContractFactory = await ethers.getContractFactory("RayzeMeal")
+        mealTokenFactory = await ethers.getContractFactory("MealToken")
+
+        // deploying the marketplace contract
+        rayze = await rayzeContractFactory.deploy(TOKEN_NAME, TOKEN_SYMBOL, NFT_NAME, NFT_SYMBOL)
+        await rayze.deployed()
+
+        // deploying the token contract
+        const tokenAddress = await rayze.mealToken()
+        mealToken = mealTokenFactory.attach(tokenAddress).connect(ethers.provider)
+
+        // deploying the NFT contract
+        const nftAddress = await rayze.rayzeMeal()
+        mealNFT = nftContractFactory.attach(nftAddress).connect(ethers.provider)
+        await mealNFT.deployed()
+    })
+
+    describe("After deploying the contracts", async () => {
+        it("Initializes the contracts properly", async () => {
+            const tokenName = await mealToken.name()
+            const tokenSymbol = await mealToken.symbol()
+            const nftName = await mealNFT.name()
+            const nftSymbol = await mealNFT.symbol()
+
+            expect(TOKEN_NAME).to.eq(tokenName)
+            expect(TOKEN_SYMBOL).to.eq(tokenSymbol)
+            expect(NFT_NAME).to.eq(nftName)
+            expect(NFT_SYMBOL).to.eq(nftSymbol)
+        })
+        it("Token supply is zero", async () => {
+            const tokenSupply = await mealToken.totalSupply()
+            expect(tokenSupply).to.eq(0)
+        })
+    })
+
+    describe("Purchase Meal Tokens", async () => {
+        it("Only accepts the right stablecoins as payment", async () => {
+            throw new Error("Not implemented!")
+        })
+        it("Reverts if the stablecoin amount sent is below minimum", async () => {
+            throw new Error("Not implemented!")
+        })
+        it("Mints the correct amount of tokens to the user's wallet", async () => {
+            throw new Error("Not implemented!")
+        })
+    })
+
+    describe("Redeem Meal Tokens", async () => {
+        it("Reverts if user's balance is 0", async () => {
+            throw new Error("Not implemented!")
+        })
+        it("Allows the user to redeem meal coins back to the stablecoin", async () => {
+            throw new Error("Not implemented!")
+        })
+    })
+
+    describe("Issue Rayze Meal (by the restaurant)", async () => {
+        it("Requires all of the fields to be properly filled. Reverts otherwise", async () => {
+            throw new Error("Not implemented!")
+        })
+        it("Successfully mints the number of meals requested to the restaurant's wallet", async () => {
+            throw new Error("Not implemented!")
+        })
+    })
+
+    describe("Withdraw proceeds (restaurant)", async () => {
+        it("Only allows the restaurant's wallet to withdraw", async () => {
+            throw new Error("Not implemented!")
+        })
+        it("Reverts if the restaurant doesn't have any proceeds", async () => {
+            throw new Error("Not implemented!")
+        })
+        it("Successfully withdraws when all conditions are satisfied. Restaurant's wallet balance increases accordingly", async () => {
+            throw new Error("Not implemented!")
+        })
+    })
+
+    describe("Balanced booked", async () => {
+        it("returns the types and numbers of active meals that a user can redeem", async () => {
+            throw new Error("Not implemented!")
+        })
+    })
+})
